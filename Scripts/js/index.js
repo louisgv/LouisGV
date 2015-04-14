f3da83cc71ab4981dac396c1b7a36c98fb50209c
Louis Graham Vichy <lgvichy@gmail.com>
2015-04-13 21:29:10 -0700

    New Main page!
    TODO: Update and polish into a new version, with backend node helper!

--- Scripts/js/index.js
+++ Scripts/js/index.js
@@ -1,1 +1,15 @@
+$(document).ready(function() {
 
+  var g = $(".greeting"),
+      time = new Date(),
+      h = time.getHours();
+  if (h>18){
+    g.text("\tGood evening,")
+  }
+  else if(h>12){
+    g.html("    Good afternoon,");
+  }
+  else {
+    g.html("    Good morning!")
+  }
+})
